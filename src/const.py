804c20d6ef83b3c3b76c3bb170ec8b508c19b3f4
ryota0051 <re0061vh@gmail.com>
2023-07-20 01:14:39 +0900

    元データcsvファイルのパス追加

--- src/const.py
+++ src/const.py
@@ -2,6 +2,11 @@ from pathlib import Path
 
 DATA_ROOT = Path("/work/data")
 
+BASE_TRAIN_CSV = DATA_ROOT / "train.csv"
+BASE_TEST_CSV = DATA_ROOT / "test.csv"
+BASE_CITE_CSV = DATA_ROOT / "cite.csv"
+SAMPLE_SUBMMISION_CSV = DATA_ROOT / "sample_submission.csv"
+
 TRAIN_CSV_WITH_IMG_PATH = DATA_ROOT / "tain_with_img_path.csv"
 CITE_CSV_WITH_IMG_PATH = DATA_ROOT / "cite_with_img_path.csv"
 TEST_CSV_WITH_IMG_PATH = DATA_ROOT / "test_with_img_path.csv"
